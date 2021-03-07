48dfcbd6181056a95bf1a9c13e603ddaa2b19f8e
Miloud Maamar <development@maamar.fr>
2021-03-07 15:28:26 +0100

    [Enums] Enum association between operator and keys

---
+++ src/app/shared/enums/operator.enum.ts
@@ -0,0 +1,8 @@
+import { NumpadKey } from '@Enums/numpad-key.enum.ts';
+
+export enum Operator {
+  Add = NumpadKey.Add,
+  Multiply = NumpadKey.Multiply,
+  Substract = NumpadKey.Substract,
+  Divide = NumpadKey.Divide,
+}
