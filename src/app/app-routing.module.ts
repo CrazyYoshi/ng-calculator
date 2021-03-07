cc28b02e024bcfb3b3d0adf9de6386299014c55b
Miloud Maamar <development@maamar.fr>
2021-03-07 15:25:54 +0100

    [Routing] Redirect all to base url + point to calculator component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from '@Calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CalculatorComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
